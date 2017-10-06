92030b1b5d341920844014abfc4961853dbf026c
aleks-lazic <aleksandar.lazic@students.hevs.ch>
2017-10-06 20:32:03 +0200

    try to add lines but async

--- ResaBike/db/line.js
+++ ResaBike/db/line.js
@@ -4,25 +4,32 @@ var models = require('../models');
 
 
 var insertLine = function(lineNumber, stationDeparture, stationTerminal, zoneName){
-    //get the id of station departure
-    let idDeparture = dbStation.getStationIdByName(stationDeparture);
-    //get the id of station terminal
-    let idTerminal = dbStation.getStationIdByName(stationTerminal);
-    //get the id of the zone
-    let idZone = dbZone.getZoneIdByName(zoneName);
-    
-    Promise.all([idDeparture, idTerminal, idZone]).then((res)=>{
-        models.Line.create({
-            number: lineNumber,
-            ZoneId: res[2],
-            DepartureId: res[0],
-            ArrivalId: res[1]
-        }).then(() =>{
-            console.log('Line added : ');
-        }).catch((err) =>{
-            console.log(err.message);
-        });
+    return new Promise((resolve, reject) => {
+        //get the id of station departure
+        let idDeparture = dbStation.getStationIdByName(stationDeparture);
+        //get the id of station terminal
+        let idTerminal = dbStation.getStationIdByName(stationTerminal);
+        //get the id of the zone
+        let idZone = dbZone.getZoneIdByName(zoneName);
+        
+        Promise.all([idDeparture, idTerminal, idZone])
+        .then((res)=>{
+            models.Line.create({
+                number: lineNumber,
+                ZoneId: res[2],
+                DepartureId: res[0],
+                ArrivalId: res[1]
+            }).then(() =>{
+                console.log('Line added : ');
+                resolve(true);
+            }).catch((err) =>{
+                console.log(err.message);
+            });
+        })
     })
+            
 }
+
+var insertOnlyLine
 
 exports.insertLine = insertLine;
--- ResaBike/routes/testDataApi.js
+++ ResaBike/routes/testDataApi.js
@@ -14,13 +14,9 @@ addLinesFromAPI();
 
 function getDataFromAPI(url){
     return new Promise((resolve, reject)=>{
-        request({
-            url: url,
-            json: true
-        }, function (error, response, body) {
+        request(url, function (error, response, body) {
             if (!error && response.statusCode === 200) {
-                var obj = JSON.stringify(body);
-                obj = JSON.parse(obj);
+                var obj = JSON.parse(body);
                 resolve(obj);
             }
         })  
@@ -44,11 +40,13 @@ function addLinesFromAPI(){
     getDataFromAPI(url)
     .then((obj) =>{
         var legs = obj.connections[0].legs;
-        for(let i = 1 ; i<legs.length ; i++){
+        let i = 1;
+ 
+        for(i = 1 ; i<legs.length ; i++){
             //si la legs est la dernière on return car il n'y a pas de line dedans
-            if(i == legs.length -1){
+            if(legs[i].stops === null)
                 return;
-            }
+            
             let stationDeparture = legs[i].name;
             let stationTerminal = legs[i].terminal;
             console.log(legs[i].line);

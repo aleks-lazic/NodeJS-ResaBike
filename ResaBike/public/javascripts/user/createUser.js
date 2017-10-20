be2207b9a191fa70273e1a3350e8c4b33c7dc931
aleks-lazic <aleksandar.lazic@students.hevs.ch>
2017-10-20 13:59:03 +0200

    create user, delete, update

--- ResaBike/public/javascripts/user/createUser.js
+++ ResaBike/public/javascripts/user/createUser.js
@@ -1,63 +1,9 @@
 $(document).ready(function() {
-
-    // for HTML5 "required" attribute
-    $("#selectRole[required]").css({display: "block", height: 0, padding: 0, width: 0, position: 'absolute'});
     
-    //récupérer tous les rôles    
-    $.ajax({
-        url : '/user/getAllRoles',
-        type : 'GET',
-        success : function(data){
-            data = JSON.parse(data);
-            var select = document.getElementById('selectRole');
-            data.forEach(function(element) {
-                var opt = document.createElement("option");
-                opt.value = element.name;
-                opt.textContent = element.name;
-                select.appendChild(opt);
-            }, this);
-        }
-    });
+   
 });
 
 function createUser(){
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var password2 = document.getElementById('password2').value;
-    var email = document.getElementById('email').value;
-    var roleIndex = document.getElementById('selectRole');
-    var role = roleIndex.options[roleIndex.selectedIndex].value; 
-
-    // //create the object to create the user
-    var user = {
-        username: username,
-        password: password,
-        password2: password2,
-        email: email,
-        role: role
-    };
-
-    $.ajax({
-        url : '/user/create',
-        type : 'POST',
-        data: user,
-        success : function(res){
-            res = JSON.parse(res);
-            console.log(typeof(res));
-            console.log(res);
-            if(res == 'success'){
-                window.location.href = '/user';
-            } else{
-                if(res == 'username'){
-                    alert("This user already exists");
-                    return;
-                }else if(res == 'password'){
-                    console.log(res);                    
-                    alert("The passwords are not matching");
-                    return;
-                }
-            }
-        }
-    });
+   
 }
--- ResaBike/public/javascripts/user/getAllUsers.js
+++ ResaBike/public/javascripts/user/getAllUsers.js
@@ -1,5 +1,6 @@
 var userName;
 
+//modal to open the update user
 function modalModifyUser(name, mail, id){
     //get inputs values
     document.getElementById('username').value = name;
@@ -13,6 +14,7 @@ function modalModifyUser(name, mail, id){
     $('#modal1').modal('open');
 }
 
+//modal to update the user
 function modifyUser(){
 
     //get values from modal's input
@@ -51,6 +53,78 @@ function modifyUser(){
     });
 }
 
+//modal to open the create user
+function modalCreateUser(){
+    //load all the roles
+     $.ajax({
+        url : '/user/getAllRoles',
+        type : 'GET',
+        success : function(data){
+            data = JSON.parse(data);
+            var select = document.getElementById('selectRole');
+            data.forEach(function(element) {
+                var opt = document.createElement("option");
+                opt.value = element.name;
+                opt.textContent = element.name;
+                select.appendChild(opt);
+            }, this);
+        }
+    });
+
+    //open modal
+    $('.modal').modal();
+    $('#modalCreateUser').modal('open');
+}
+
+//create the user on create button
+function createUser(){
+
+    let username = document.getElementById('usernamee').value;
+    var password = document.getElementById('passwordd').value;
+    var password2 = document.getElementById('passwordd2').value;
+    var email = document.getElementById('emaill').value;
+    var roleIndex = document.getElementById('selectRole');
+    var role = roleIndex.options[roleIndex.selectedIndex].value; 
+
+    console.log("username : " + username);
+
+    // //create the object to create the user
+    var user = {
+        username: username,
+        password: password,
+        password2: password2,
+        email: email,
+        role: role
+    };
+    
+    $.ajax({
+        url : '/user/create',
+        type : 'POST',
+        data: user,
+        success : function(res){
+            res = JSON.parse(res);
+            console.log(typeof(res));
+            console.log(res);
+            if(res == 'success'){
+                $('.modal').modal();                
+                $('#modalCreateUser').modal('close');                
+                $('#usersTable').load(' #usersTable');
+                $('#formCreateUser')[0].reset();                                
+            } else{
+                if(res == 'username'){
+                    alert("This user already exists");
+                    return;
+                }else if(res == 'password'){
+                    console.log(res);                    
+                    alert("The passwords are not matching");
+                    return;
+                }
+            }
+        }
+    });
+}
+
+//delete the user by id
 function deleteUser(id){
     //ajax DELETE
     $.ajax({

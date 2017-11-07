0e60be8df9315e5f4efa503769e85e13f221d822
aleks-lazic <aleksandar.lazic@students.hevs.ch>
2017-11-07 17:17:49 +0100

    resetDB

--- ResaBike/ressources/resetDB.js
+++ ResaBike/ressources/resetDB.js
@@ -1,8 +1,17 @@
 var models = require('../models');
 var dbZone = require('../db/zone');
+var dbRole = require('../db/role');
+var dbUser = require('../db/user');
 
 models.sequelize.sync({force:true}).then(function () {
+    var promises = [];
     //rajouter tous les trucs
-    dbZone.createZone('Anniviers');
+    promises.push(dbRole.createRole(1, 'chauffeur'));
+    promises.push(dbRole.createRole(2, 'zoneadmin'));
+    promises.push(dbRole.createRole(3, 'sysadmin'));
+    
+    Promise.all(promises).then(() => {
+        dbUser.createUser('root', 'root', 'root@root.com', 3);
+    })
 
 });
